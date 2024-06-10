93acb72a829e3185d1b3f995a56627dae90a70d0
Alex Luedtke <aluedtke@MacBook-Pro-185.local>
2024-06-10 15:32:35 -0700

    updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,9 @@ extensions = ['sphinx.ext.autodoc']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../pydimple'))
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
