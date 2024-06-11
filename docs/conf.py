5fb9a41e0b28fe20e48e5e0ec9b23e50b442bf35
Alex Luedtke <aluedtke@MacBook-Pro-185.local>
2024-06-10 17:15:12 -0700

    updating docs

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,4 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
-# -- Mock imports -------------------------------------------------------------
-# Mock the import of 'funs' module
-autodoc_mock_imports = ['funs']
