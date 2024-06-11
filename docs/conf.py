0d14ef5362a4de97a7e1f7829e1927f6434a6762
Alex Luedtke <aluedtke@MacBook-Pro-185.local>
2024-06-11 07:49:56 -0700

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = 'v0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc','myst_parser']
+extensions = ['sphinx.ext.autodoc', 'myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -39,3 +39,5 @@ source_suffix = {
     '.rst': 'restructuredtext',
     '.md': 'markdown',
 }
+
+myst_enable_extensions = ["amsmath"]
